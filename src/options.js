c4715e6ea3951787a1aa4e488a9035e8ad460767
Pete Schlette <pete.schlette@gmail.com>
2017-10-03 05:23:53 +0000

    Remove beeper-specific code (that's most of it)

--- src/options.js
+++ src/options.js
@@ -2,11 +2,7 @@
 
 // Saves options to chrome.storage.sync.
 const save_options = () => {
-  var fallbackGracePeriodSeconds = 5;
-  var rawTurnNotifierGracePeriodSeconds= document.getElementById('turnNotifierGracePeriodSeconds').value;
-  var turnNotifierGracePeriodSeconds = parseInt(rawTurnNotifierGracePeriodSeconds, 10) || fallbackGracePeriod;
   chrome.storage.sync.set({
-    turnNotifierGracePeriodSeconds: turnNotifierGracePeriodSeconds
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -21,9 +17,7 @@ const save_options = () => {
 // stored in chrome.storage.
 const restore_options = () => {
   chrome.storage.sync.get({
-    turnNotifierGracePeriodSeconds: '5'
   }, function(items) {
-    document.getElementById('turnNotifierGracePeriodSeconds').value = items.turnNotifierGracePeriodSeconds;
   });
 }
 
